f655ba8aa4518dd06b7c684ec8295ed0079c1ef7
Kinfe <kinfe@2534@gmail.com>
2021-05-03 07:27:52 +0300

    added json Server

---
+++ restaurantData.js
@@ -0,0 +1,16 @@
+module.exports = function () {
+    var data = {
+        restaurants: [
+            {id:1,name: 'Neba', location:'Goro',category:'Fast Food',openingHour:null,menu:null,logoPath:"assets/feather/activity.svg"},
+            {id:2, name:'Pizeria',location: 'Bole Medhanialem',category:'Piza Bet',openingHour:null,menu:null,logoPath:"assets/feather/airplay.svg"},
+            {id:3, name:'Totot',location: 'Gerji',category:'Fast Food',logoPath:"assets/feather/anchor.svg"},
+            {id:4, name: 'Jacaranda',location: 'Paster',category:'Cafe',logoPath:"assets/feather/aperture.svg"},
+            {id:5, name:'Yonas Chercher', location:'Kazanchis',category:'Buchery',logoPath:"assets/feather/briefcase.svg"},
+            {id:6,name: 'Coffee Days',location:  'Kazanchis',category:'Cafe',logoPath:"assets/feather/align-center.svg"},
+            {id:7, name:'H_square Supermarket',location:  'Only Helina Knows',category:'Supermarket',logoPath:"assets/feather/align-center.svg"},
+            {id:8,name: 'Yedi ',location:  'Goro',category:'Traditional Shiro',logoPath:"assets/feather/align-left.svg"}
+            
+        ]
+    }
+    return data
+}
